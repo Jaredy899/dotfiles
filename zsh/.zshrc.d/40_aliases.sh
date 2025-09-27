90c014724f5e6077ce47bef1079b5e1332d1c8d1
Jared Cervantes <jared@jaredcervantes.com>
2025-09-27 15:36:39 -0400

    edits

--- zsh/.zshrc.d/40_aliases.sh
+++ zsh/.zshrc.d/40_aliases.sh
@@ -75,7 +75,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
   alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history | tail -n1 | sed -e "s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//")"'
 fi
-alias zshrc='${EDITOR} ~/.local/share/dotfiles/zsh/.zshrc.d'
+alias ezsh='${EDITOR} ~/dotfiles/zsh/.zshrc.d'
 
 # Platform-specific aliases
 if [[ "$OSTYPE" == "darwin"* ]]; then
